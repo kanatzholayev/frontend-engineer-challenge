e738aed1b3a24d226c5c79818c39f3ec65adf1da
Kanat Zholayev <jolaev.k@gmail.com>
2026-04-10 17:21:01 +0500

    feat: add registration form component and integrate it into the login page with toggle functionality

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,26 +1,49 @@
+import { useState } from 'react';
+
 import { IMAGES } from '@/shared/assets';
 import { Button } from '@/shared/ui';
 import { LoginForm } from '@/features/login';
+import { RegisterForm } from '@/features/register';
 
 import styles from './LoginPage.module.scss';
 
-export const LoginPage = () => (
-  <div className={styles.container}>
-    <div className={styles.content}>
-      <img src={IMAGES.logo} alt="logo" className={styles.logo} />
+export const LoginPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
 
-      <LoginForm />
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img src={IMAGES.logo} alt="logo" className={styles.logo} />
 
-      <div className={styles.footer}>
-        Еще не зарегистрированы?
-        {' '}
+        {isLogin
 
-        <Button variant="tertiary" className={styles.registerButton}>Регистрация</Button>
+          ? (
+            <>
+              <LoginForm />
+
+              <div className={styles.footer}>
+                Еще не зарегистрированы?
+
+                <Button variant="tertiary" className={styles.registerButton} onClick={() => setIsLogin(false)}>Регистрация</Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <RegisterForm />
+
+              <div className={styles.footer}>
+                Уже зарегистрированы?
+
+                <Button variant="tertiary" className={styles.registerButton} onClick={() => setIsLogin(true)}>Войти</Button>
+              </div>
+            </>
+          )}
+
+      </div>
+
+      <div className={styles.imageContainer}>
+        <img src={IMAGES.authImage} alt="auth" className={styles.image} />
       </div>
     </div>
-
-    <div className={styles.imageContainer}>
-      <img src={IMAGES.authImage} alt="auth" className={styles.image} />
-    </div>
-  </div>
-);
+  );
+};
